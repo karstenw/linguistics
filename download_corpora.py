fe96bc77bf310e38f4d1e7598849ef8907ae721d
Karsten Wolf <karstenwo@web.de>
2026-05-19 22:52:42 +0200

    Update download_corpora.py

--- download_corpora.py
+++ download_corpora.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 import pdb
-kwdbg = False
+kwdbg = True
 
 
 import pprint
@@ -12,8 +12,10 @@ import time
 
 start = time.time()
 
+nb=True
 try:
     PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+    nb=False
 except NameError as err:
     print(err)
     PACKAGE_DIR = os.path.abspath( './' )
@@ -29,28 +31,46 @@ if not os.path.exists( DATA_DIR ):
 
 sys.path.insert(0, PACKAGE_DIR)
 
+# pdb.set_trace()
+
+# if run from Nodebox do not accumulate endless messages...
+# therefore 
+#       quiet=nb
+# or
+#       progress_handler=pg
+
 # textblob uses nltk
 import nltk
 nltk_data_dir = os.path.join( DATA_DIR, 'nltk-data' )
 nltk.data.path = [ nltk_data_dir ]
 
-nltk.download( "wordnet_ic", download_dir=nltk_data_dir )
-nltk.download( "wordnet", download_dir=nltk_data_dir )
-nltk.download( "sentiwordnet", download_dir=nltk_data_dir )
+nltk.download( "wordnet", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "wordnet_ic", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "sentiwordnet", download_dir=nltk_data_dir, quiet=nb )
+
+nltk.download( "wordnet2021", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "wordnet2022", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "wordnet31", download_dir=nltk_data_dir, quiet=nb )
+
 
 # textblob downloads
-nltk.download( "brown", download_dir=nltk_data_dir )
-nltk.download( "punkt", download_dir=nltk_data_dir )
-nltk.download( "averaged_perceptron_tagger", download_dir=nltk_data_dir )
-nltk.download( "conll2000", download_dir=nltk_data_dir )
-nltk.download( "movie_reviews", download_dir=nltk_data_dir )
+nltk.download( "brown", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "punkt", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "averaged_perceptron_tagger", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "conll2000", download_dir=nltk_data_dir, quiet=nb )
+nltk.download( "movie_reviews", download_dir=nltk_data_dir, quiet=nb )
 
 
 import wn
+from wn.util import ProgressHandler, ProgressBar
+
+pg = ProgressBar
+if nb:
+    pg = ProgressHandler
 wn.config.data_directory = os.path.join( DATA_DIR, 'wn-data' )
-wn.download("omw")
-wn.download("odenet")
-wn.download("cili")
+wn.download("omw", progress_handler=pg)
+wn.download("odenet", progress_handler=pg)
+wn.download("cili", progress_handler=pg)
 
 stop = time.time()
 print("nltk & wn in %.3fsec" % (stop-start,) )
